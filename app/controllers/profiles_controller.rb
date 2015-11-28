79bc96bf3ad7bfd4ea3fe2d9857ef3103013f29c
vjandrei <andreas.koutsoukos@gmail.com>
2015-11-28 10:53:37 +0200

    update

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,7 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /profiles
   # GET /profiles.json
@@ -14,7 +16,7 @@ class ProfilesController < ApplicationController
 
   # GET /profiles/new
   def new
-    @profile = Profile.new
+    @profile = current_user.profiles.build
   end
 
   # GET /profiles/1/edit
@@ -24,7 +26,7 @@ class ProfilesController < ApplicationController
   # POST /profiles
   # POST /profiles.json
   def create
-    @profile = Profile.new(profile_params)
+    @profile = current_user.profiles.build(profile_params)
 
     respond_to do |format|
       if @profile.save
@@ -66,6 +68,11 @@ class ProfilesController < ApplicationController
     def set_profile
       @profile = Profile.find(params[:id])
     end
+    
+    def correct_user
+	    @profile = current_user.profiles.find_by(id: params[:id])
+		redirect_to profiles_path, notice: "Not authorized to edit this pin" if @profile.nil?
+	end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def profile_params
