f667d2c42db69225bf42000527996891fe1d1688
vjandrei <andreas.koutsoukos@gmail.com>
2015-12-02 09:25:37 +0200

    capistrano update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,8 @@
+set :stage, :production
+
+# Replace 127.0.0.1 with your server's IP address!
+server '162.243.240.140', user: 'deploy', roles: %w{web app}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
