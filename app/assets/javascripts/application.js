2479bca839c0b94ff59abe13ca0ccd09f15bc4e1
vjandrei <andreas.koutsoukos@gmail.com>
2015-12-04 13:12:25 +0200

    mobile upload

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,20 @@
 //= require turbolinks
 //= require_tree .
 
+
+// Detect file input support
+var isFileInputSupported = (function () {
+  // Handle devices which falsely report support
+  if (navigator.userAgent.match(/(Android (1.0|1.1|1.5|1.6|2.0|2.1))|(Windows Phone (OS 7|8.0))|(XBLWP)|(ZuneWP)|(w(eb)?OSBrowser)|(webOS)|(Kindle\/(1.0|2.0|2.5|3.0))/)) {
+    return false;
+  }
+  // Create test element
+  var el = document.createElement("input");
+  el.type = "file";
+  return !el.disabled;
+})();
+
+// Add 'fileinput' class to html element if supported
+if (isFileInputSupported) {
+  document.documentElement.className += " fileinput";
+}
