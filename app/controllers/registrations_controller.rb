24327e19869a59d3ad0286b277a25b1eb43ee9f1
vjandrei <andreas.koutsoukos@gmail.com>
2015-12-06 14:49:07 +0200

    changes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    '/profiles/' # Or :prefix_to_your_route
+    root_path # Or :prefix_to_your_route
   end
 end
