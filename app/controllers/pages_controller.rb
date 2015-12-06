375a48df3b40b5f28d557a26222c805fc4fe1dd8
vjandrei <andreas.koutsoukos@gmail.com>
2015-12-06 15:56:07 +0200

    ui fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
 
   	if params[:category].blank?
-		@profiles = Profile.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 2)
+		@profiles = Profile.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 30)
 	else
 		@category_id = Category.find_by(name: params[:category]).id
 		@profile = Profile.where(category_id: @category_id).order("created_at DESC")
